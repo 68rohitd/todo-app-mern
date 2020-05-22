e635a1ada021875f861c35061c93604ec8d71e5d
Rohit D <6rohit8@gmail.com>
2020-05-23 01:51:29 +0530

    bug fix

---
+++ client/build/precache-manifest.c534fe8818b690efba7c6201c4d51d31.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b231b090fbc20550088d9b78c4c641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf771b3775c629b5668",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "85327e05acca1807bd4a",
+    "url": "/static/css/main.923477de.chunk.css"
+  },
+  {
+    "revision": "acf771b3775c629b5668",
+    "url": "/static/js/2.0079a186.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0079a186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85327e05acca1807bd4a",
+    "url": "/static/js/main.fd443dbc.chunk.js"
+  },
+  {
+    "revision": "2800fcddd66813d244bf",
+    "url": "/static/js/runtime-main.5db5293e.js"
+  },
+  {
+    "revision": "331ac8f6b2fe8f4a4f0860863fbff7e9",
+    "url": "/static/media/login.331ac8f6.svg"
+  },
+  {
+    "revision": "a7f9007647dcec4ee57ac9c8048c7d24",
+    "url": "/static/media/login3.a7f90076.svg"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/pageNotFound.6695af99.svg"
+  }
+]);
