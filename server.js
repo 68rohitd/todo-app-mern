a2aa15fff2f04ff56dbc29d0e47169c21b8d42ad
Rohit D <6rohit8@gmail.com>
2020-05-23 12:24:16 +0530

    bug fix

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    rs.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 const port = process.env.PORT || 5000;
