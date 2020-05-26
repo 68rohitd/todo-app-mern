78d6190f8b3d2b4f8f32d8300762681b06826fd8
Rohit D <6rohit8@gmail.com>
2020-05-26 15:33:39 +0530

    added speech synthesis

--- client/src/components/todo/Todo.jsx
+++ client/src/components/todo/Todo.jsx
@@ -8,6 +8,15 @@ import { Spring } from "react-spring/renderprops";
 import "../../assets/todo-styles/todo.css";
 
 class Todo extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playing: false,
+    };
+
+    this.synth = window.speechSynthesis;
+  }
   onDelete(id, dispatch) {
     axios
       .delete(`/todos/${id}`)
@@ -90,6 +99,56 @@ class Todo extends Component {
     });
   };
 
+  onSpeak = (todoItem) => {
+    if (!this.synth.speaking) {
+      const speed = 1.5;
+      this.setState(
+        {
+          playing: true,
+        },
+        () => {
+          const sent = new SpeechSynthesisUtterance(todoItem.title);
+          sent.rate = speed;
+
+          this.synth.speak(sent);
+
+          const end = todoItem.todoName.length - 1;
+          todoItem.todoName.forEach((task, index) => {
+            const sent = new SpeechSynthesisUtterance(task.itemName);
+            sent.rate = speed;
+            this.synth.speak(sent);
+
+            sent.onend = () => {
+              if (index === end) {
+                const sent = new SpeechSynthesisUtterance(
+                  "That's all for now!"
+                );
+                sent.rate = speed;
+                this.synth.speak(sent);
+                this.setState({
+                  playing: false,
+                });
+              }
+            };
+          });
+        }
+      );
+    } else {
+      if (this.state.playing === true) {
+        this.synth.cancel();
+        this.setState({
+          playing: false,
+        });
+      }
+    }
+  };
+
+  componentWillUnmount() {
+    if (this.state.playing) {
+      this.synth.cancel();
+    }
+  }
+
   render() {
     const { todoItem } = this.props;
 
@@ -123,36 +182,48 @@ class Todo extends Component {
                   <div className="myCard card pb-1" style={{ ...styles.card }}>
                     <div className="card-header pt-2 pb-2">
                       <div className="row">
+                        {/* collapse icon */}
                         <div className="col-1">
-                          {/* collapse icon */}
                           <i
-                            className={classNames("myIcon", {
-                              "fa fa-angle-down": todoItem.collapsed,
-                              "fa fa-angle-up": !todoItem.collapsed,
+                            className={classNames("myIcon collapseIcon fa", {
+                              "fa-angle-down": todoItem.collapsed,
+                              "fa-angle-up": !todoItem.collapsed,
                             })}
                             onClick={() => this.onCollapsed(todoItem, dispatch)}
                           ></i>
                         </div>
 
+                        {/* date info */}
                         <div className="col">
-                          {/* date info */}
                           <small className="text-muted">
                             {this.onGetTime(todoItem.createdAt)}
                           </small>
                         </div>
 
-                        <div className="col-3">
-                          {/* status of task: new, in progress, completed */}
+                        {/* todo status */}
+                        <div className="col-2">
                           <span
+                            style={{ float: "right" }}
                             className="badge badge-success"
-                            style={{ borderRadius: "7px" }}
                           >
                             {todoItem.status}
                           </span>
                         </div>
 
+                        {/* speech syn */}
                         <div className="col-1">
-                          {/* mark as complete icon */}
+                          <i
+                            data-tip="speak out loud"
+                            className={classNames("myIcon fa", {
+                              "fa-play": this.state.playing === false,
+                              "fa-stop": this.state.playing === true,
+                            })}
+                            onClick={() => this.onSpeak(todoItem)}
+                          ></i>
+                        </div>
+
+                        {/* mark as complete icon */}
+                        <div className="col-1">
                           <ReactTooltip place="bottom" delayShow={1000} />
 
                           <i
@@ -167,8 +238,8 @@ class Todo extends Component {
                           ></i>
                         </div>
 
+                        {/* edit icon */}
                         <div className="col-1">
-                          {/* edit icon */}
                           <Link to={`/edit/${todoItem._id}`}>
                             <i
                               data-tip="edit task"
@@ -177,10 +248,10 @@ class Todo extends Component {
                           </Link>
                         </div>
 
+                        {/* delete icon */}
                         <div className="col-1">
-                          {/* delete icon */}
                           <i
-                            className="myIcon fa fa-times text-danger"
+                            className="myIcon delIcon fa fa-times text-danger"
                             onClick={this.onDelete.bind(
                               this,
                               todoItem._id,
